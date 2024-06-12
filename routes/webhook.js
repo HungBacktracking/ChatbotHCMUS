4460e5e38d9eb9a25124cd00d21309541308d83d
Võ Trung Hoàng Hưng <21120011@student.hcmus.edu.vn>
2024-06-13 05:26:52 +0700

    add package and eslint

--- routes/webhook.js
+++ routes/webhook.js
@@ -5,22 +5,22 @@ const config = require('../config');
 const router = Router();
 
 router.get('/', (req, res) => {
-  // Parse the query params
-  const mode = req.query['hub.mode'];
-  const token = req.query['hub.verify_token'];
-  const challenge = req.query['hub.challenge'];
+	// Parse the query params
+	const mode = req.query['hub.mode'];
+	const token = req.query['hub.verify_token'];
+	const challenge = req.query['hub.challenge'];
 
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === config.PAGE_VERIFY_TOKEN) {
-      // Responds with the challenge token from the request
-      res.status(200).send(challenge);
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
-    }
-  }
+	// Checks if a token and mode is in the query string of the request
+	if (mode && token) {
+		// Checks the mode and token sent is correct
+		if (mode === 'subscribe' && token === config.PAGE_VERIFY_TOKEN) {
+		// Responds with the challenge token from the request
+		res.status(200).send(challenge);
+		} else {
+		// Responds with '403 Forbidden' if verify tokens do not match
+		res.sendStatus(403);
+		}
+	}
 });
 
 router.post('/', verifyXHub, (req, res) => {
