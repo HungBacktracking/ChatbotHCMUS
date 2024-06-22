20e850dd259f9af96372df6821587859b9669cf5
Hoang C. Ta <taconghoang1103@gmail.com>
2024-06-22 15:59:51 +0700

    add startConversation function

--- services/llm_service.js
+++ services/llm_service.js
@@ -1,11 +1,64 @@
-const { GoogleGenerativeAI } = require('@google/generative-ai');
+const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require('@google/generative-ai');
 const config = require('../config');
 const logger = require('../utils/logger'); 
+import { introduction_HCMUS } from '../database/HCMUS';
 
 const genAI = new GoogleGenerativeAI(config.GEMINI_API);
 const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
-// text: The text prompt to generate a response for
+// const SAFETY_SETTINGS = [
+//     { category: HarmCategory.HARM_CATEGORY_HARASSMENT, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE },
+//     { category: HarmCategory.HARM_CATEGORY_HATE_SPEECH, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE },
+//     { category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE },
+//     { category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE },
+// ]; // Default safety settings
+
+// const GENERATION_CONFIG = {
+//     max_tokens: 4096,
+//     temperature: 0.3, // control the randomness of the output
+//     top_p: 1.0, // control the diversity of the output
+//     frequency_penalty: 0.0, // control the diversity of the output
+//     presence_penalty: 0.0, // control the repetitiveness of the output
+//     best_of: 1, // generate multiple completions and return the best one
+// };
+
+// async function startConversation() {
+//     try {
+//         const chat = model.startChat({
+//             generationConfig: GENERATION_CONFIG,
+//             safetySettings: SAFETY_SETTINGS,
+//             history: [
+//                 { role: "user", content: "Bạn có biết về trường Đại học Khoa học Tự nhiên không?"},
+//                 { role: "model", content: "Có phải ý bạn là trường Đại học Khoa học Tự nhiên - ĐHQG TPHCM không?"},
+//                 { role: "user", content: "Đúng rồi, bạn có thể cho mình biết về trường này được không?"},
+//                 { role: "model", content: $(introduction_HCMUS)}, 
+//             ], 
+
+//         });
+
+//         while (true) {
+//             const userMessage = await getUserMessage();
+            
+//             if(userMessage.toLowerCase() === 'exit' || userMessage.toLowerCase() === 'thoát' || 
+//                         userMessage.toLowerCase() === 'kết thúc' || userMessage.toLowerCase() === 'dừng lại') {
+//                 break;
+//             }
+            
+//             const result = await chat.sendMessage(userMessage);
+            
+//             if (result.error) {
+//                 logger.logError('LLM::startConversation', 'Failed to generate AI response ' + result.error);
+//                 continue;
+//             }
+//             const response = result.response();
+
+//             const text = Buffer.from(response.text(), 'utf-8').toString();
+//             logger.logError('LLM::startConversation', 'Content: ' + text);
+//         }
+//     } catch (error) {
+//         logger.logError('LLM::startConversation', 'Failed to generate AI response ' + config.GEMINI_API, error, true);
+//     }
+// }
 
 const generateResponse = async (command) => {
     try {
