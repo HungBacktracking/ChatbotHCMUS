4760c40099345e50f0c21523c782a38ef9178e56
Hoang C. Ta <taconghoang1103@gmail.com>
2024-07-04 21:52:23 +0700

    test club 02

--- services/club.js
+++ services/club.js
@@ -1,3 +1,4 @@
+const fb = require('../utils/facebook');
 
 const club = [
     { name : "HCMUS Football Club", description: "Trang thông tin chính thức của CLB Bóng Đá Trường ĐH Khoa Học Tự Nhiên - ĐHQG HCM.", link: "https://www.facebook.com/HCMUSFC" },
