dc742131d87e6d2d7091f3bf383909e852ca4136
Hoang C. Ta <taconghoang1103@gmail.com>
2024-07-05 12:29:51 +0700

    Update img club

--- services/club.js
+++ services/club.js
@@ -1,45 +1,57 @@
 const fb = require('../utils/facebook');
-const config = require('../config');
+// const config = require('../config');
 
-const axios = require('axios');
+// const axios = require('axios');
 
-async function getPageId(pageUsername, accessToken) {
-    const url = `https://graph.facebook.com/v12.0/${pageUsername}?fields=id&access_token=${accessToken}`;
-    try {
-        const response = await axios.get(url);
-        return response.data.id;
-    } catch (error) {
-        console.error("Error fetching Page ID:", error);
-        return null;
-    }
-}
+// async function getPageId(pageUsername, accessToken) {
+//     const url = `https://graph.facebook.com/v12.0/${pageUsername}?fields=id&access_token=${accessToken}`;
+//     try {
+//         const response = await axios.get(url);
+//         return response.data.id;
+//     } catch (error) {
+//         console.error("Error fetching Page ID:", error);
+//         return null;
+//     }
+// }
 
 const club = [
-    { name : "HCMUS Football Club", description: "Trang thông tin chính thức của CLB Bóng Đá Trường ĐH Khoa Học Tự Nhiên - ĐHQG HCM.", link: "https://www.facebook.com/HCMUSFC" },
-    { name : "CLB Cờ Trường ĐH Khoa học tự nhiên, ĐHQG - HCM", description: "Trang thông tin chính thức của CLB Cờ Trường ĐH Khoa học tự nhiên, ĐHQG - HCM.", link: "https://www.facebook.com/hcmuschessclub" },
+    { 
+        name : "HCMUS Football Club", 
+        description: "Trang thông tin chính thức của CLB Bóng Đá Trường ĐH Khoa Học Tự Nhiên - ĐHQG HCM.", 
+        link: "https://www.facebook.com/HCMUSFC",
+        imgUrl: "https://www.facebook.com/photo/?fbid=521390579788569&set=a.521390549788572" 
+    },
+    { 
+        name : "CLB Cờ Trường ĐH Khoa học tự nhiên, ĐHQG - HCM", 
+        description: "Trang thông tin chính thức của CLB Cờ Trường ĐH Khoa học tự nhiên, ĐHQG - HCM.", 
+        link: "https://www.facebook.com/hcmuschessclub",
+        imgUrl: "https://www.facebook.com/photo/?fbid=228217490269019&set=a.119752341115535" 
+    },
     
 ];
 
-async function fetchImageUrl(pageId, accessToken) {
-    const url = `https://graph.facebook.com/v12.0/${pageId}/picture?type=large&redirect=false&access_token=${accessToken}`;
-    try {
-        const response = await axios.get(url);
-        return response.data.data.url;
-    } catch (error) {
-        console.error("Failed to fetch image URL", error);
-        return null;
-    }
-}
+// async function fetchImageUrl(pageId, accessToken) {
+//     const url = `https://graph.facebook.com/v12.0/${pageId}/picture?type=large&redirect=false&access_token=${accessToken}`;
+//     try {
+//         const response = await axios.get(url);
+//         return response.data.data.url;
+//     } catch (error) {
+//         console.error("Failed to fetch image URL", error);
+//         return null;
+//     }
+// }
 
 async function suggestClub(sender) {
     const elements = await Promise.all(club.map(async club => {
-        const pageUsername = club.link.split('https://www.facebook.com/')[1];
-        const pageId = await getPageId(pageUsername, config.ACCESS_TOKEN);
-        const imageUrl = await fetchImageUrl(pageId, config.ACCESS_TOKEN); 
+
+        // const pageUsername = club.link.split('https://www.facebook.com/')[1];
+        // const pageId = await getPageId(pageUsername, config.ACCESS_TOKEN);
+        // const imageUrl = await fetchImageUrl(pageId, config.ACCESS_TOKEN); 
+        
         return {
             title: club.name,
             subtitle: club.description,
-            image_url: imageUrl,
+            image_url: club.imgUrl,
             buttons: [
                 {
                     type: 'web_url',
