08a95714bb4dd3c77108fde3150fc505b1f12089
Hoang C. Ta <taconghoang1103@gmail.com>
2024-07-05 12:43:32 +0700

    delete access token
    done show club

--- config/index.js
+++ config/index.js
@@ -104,7 +104,7 @@ const config = {
     // ID of developer's Facebook account
     DEV_ID: parseEnvString('DEV_ID') || '',
 
-    ACCESS_TOKEN: parseEnvString('ACCESS_TOKEN') || '',
+    // ACCESS_TOKEN: parseEnvString('ACCESS_TOKEN') || '',
 
     // Project version.
     VERSION: '1.0.0',
